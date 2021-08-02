38aa664b26e0a9c09ff0d7284313bfe88a69def5
Aleksey <at.8441451@yandex.ru>
2021-08-02 21:08:53 +0300

    Вынес главную в отд компонент, в App - только роутинг

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,47 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import { updateIngredientsList } from '../../services/actions/burgerIngredients';
+import MainPage from '../../pages/main-page/main-page';
 
-import Modal from '../modal/modal';
-import AppHeader from '../app-header/app-header';
-import PageTitle from '../page-title/page-title';
-import BurgerIngredients from '../burger-ingredients/burger-ingredients';
-import BurgerConstructor from '../burger-constructor/burger-constructor';
-
-import styles from './app.module.css';
-
-const App = () => {
-    const dispatch = useDispatch();
-
-    const [activeTab, setTab] = useState('bun');
-
-    useEffect(
-        () => {
-            dispatch(updateIngredientsList());
-        },
-        [dispatch]
-    );
-
-    return (
-        <div className={styles.wrapper}>
-            <AppHeader />
-            <DndProvider backend={HTML5Backend}>
-                <main className={styles.main}>
-                    <PageTitle />
-                    <BurgerIngredients
-                        activeTab={activeTab}
-                        onChangeTab={setTab}
-                    />
-                    <BurgerConstructor />
-                </main>
-            </DndProvider>
-            <Modal />
-            <div id="react-modals" />
-        </div>
-    );
-};
+const App = () => (
+    <Router>
+        <Switch>
+            <Route path="/">
+                <MainPage />
+            </Route>
+        </Switch>
+    </Router>
+);
 
 export default App;
---
+++ src/pages/main-page/main-page.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { updateIngredientsList } from '../../services/actions/burgerIngredients';
+
+import styles from './app.module.css';
+
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
+import AppHeader from '../../components/app-header/app-header';
+import PageTitle from '../../components/page-title/page-title';
+import BurgerIngredients from '../../components/burger-ingredients/burger-ingredients';
+import BurgerConstructor from '../../components/burger-constructor/burger-constructor';
+import Modal from '../../components/modal/modal';
+
+const MainPage = () => {
+    const dispatch = useDispatch();
+
+    const [activeTab, setTab] = useState('bun');
+
+    useEffect(
+        () => {
+            dispatch(updateIngredientsList());
+        },
+        [dispatch]
+    );
+
+    return (
+        <div className={styles.wrapper}>
+            <AppHeader />
+                <DndProvider backend={HTML5Backend}>
+                    <main className={styles.main}>
+                        <PageTitle />
+                        <BurgerIngredients
+                            activeTab={activeTab}
+                            onChangeTab={setTab}
+                        />
+                        <BurgerConstructor />
+                    </main>
+                </DndProvider>
+            <Modal />
+            <div id="react-modals" />
+        </div>
+    );
+};
+
+export default MainPage;
