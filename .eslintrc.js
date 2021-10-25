58e25219752d864e16dca282133b2cea62d1afd6
Aleksey Trubitsyn <at.8441451@yandex.ru>
2021-10-25 18:56:18 +0300

    add typescript, eslint & prettier auto fix to the project

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    extends: [
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    rules: {
+        'react/jsx-first-prop-new-line': [2, 'multiline'],
+        'react/jsx-max-props-per-line': [2, { maximum: 1, when: 'multiline' }],
+        'react/jsx-indent-props': [4, 4],
+        'react/jsx-closing-bracket-location': [2, 'tag-aligned'],
+    },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: true,
+    trailingComma: "all",
+    singleQuote: true,
+    jsxSingleQuote: false,
+    printWidth: 80,
+    tabs: false,
+    tabWidth: 4
+};
